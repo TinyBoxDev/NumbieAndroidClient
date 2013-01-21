76e2fae114f2a58ff3ec5d68643fd77bd35dd93e
Antonio Bevilacqua <b3by.in.th3.sky@gmail.com>
2013-01-21 23:39:27 +0100

    project and test prorject added

---
+++ Numbie/src/com/tinybox/numbie/NumbieMainActivity.java
@@ -0,0 +1,20 @@
+package com.tinybox.numbie;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class NumbieMainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbie_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_numbie_main, menu);
+        return true;
+    }
+}
